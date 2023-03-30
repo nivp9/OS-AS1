d1a8072057c0fb099809e4e80ced2daa6ccb540c
nivp9 <hagainivp@mail.tau.ac.il>
2023-03-30 15:53:39 +0300

    next commit

--- pt.c
+++ pt.c
@@ -3,7 +3,6 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn){
     int i;
     int entrynumber;
     uint64_t *tablePointer = phys_to_virt(pt<<12);              //given page number add offset to create address
-    uint64_t ppn;
     for(i=0;i<4;i++){
         uint64_t mask=512-1;                                    //9-bits for 512 entries
         int maskoffset = ((57)-9*(i+1));                        // ignore sign-extend
@@ -24,6 +23,30 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn){
     else{
         tablePointer[entrynumber] = (tablePointer[entrynumber]>>1)<<1;//turn of last bit
     }
+
+}
+
+
+uint64_t page_table_query(uint64_t pt, uint64_t vpn){
+    int i;
+    int entrynumber;
+    uint64_t *tablePointer = phys_to_virt(pt<<12);              //given page number add offset to create address
+    uint64_t res;
+    for(i=0;i<5;i++){
+        uint64_t mask=512-1;                                    //9-bits for 512 entries
+        int maskoffset = ((57)-9*(i+1));                        // ignore sign-extend
+        mask = mask << maskoffset;
+        entrynumber=(vpn & mask)>>maskoffset;
+        if(!isvalid(tablePointer[entrynumber])){                //faced invalid entry
+           return NO_MAPPING;
+        }
+        if(i<4)
+            tablePointer=phys_to_virt(tablePointer[entrynumber&((~0)<<12)]); //entry without 12 first bits
+        else{
+            res= tablePointer[entrynumber]>>12;
+        }
+    }
+    return res;
 
 }
 int isvalid(uint64_t entry){
